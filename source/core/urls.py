6c777b2cda8db65f54db8ca2c1c7c71b81f5b485
Andrey Bogomolov <fluder.tw@gmail.com>
2013-09-28 14:59:12 +0300

    Added mocked version of new_rounds feeder

--- source/core/urls.py
+++ source/core/urls.py
@@ -5,4 +5,6 @@ urlpatterns = patterns(
     'core.views',
 
     url(r'^$', TemplateView.as_view(template_name='core/game_creator_page.html')),
+
+    url(r'^game/(?P<game_pk>\d+)/round/(?P<last_round_number>\d+)/$', 'new_rounds_feed', name='core-new-rounds-feed')
 )
