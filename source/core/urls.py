5dd6b5556c2de4a845bd1063d940a202ac9a3bb6
Dmytro Popovych <drudim@ukr.net>
2013-09-28 11:12:33 +0300

    Base inspiring page was added

--- source/core/urls.py
+++ source/core/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 urlpatterns = patterns(
     'core.views',
 
-    url(r'^$', TemplateView.as_view(template_name='core/hello_world.html')),
+    url(r'^$', TemplateView.as_view(template_name='core/game_creator.html')),
 )
