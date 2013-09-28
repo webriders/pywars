07a82ddb55f147622d642c45aa6fdfb32533ea7e
Dmytro Popovych <drudim@ukr.net>
2013-09-28 15:08:30 +0300

    Test page was added

--- source/core/urls.py
+++ source/core/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns(
     'core.views',
 
     url(r'^$', 'start_game_page', name='core-start-game-page'),
     url(r'^game/(?P<pk>\d+)/$', 'game_page', name='core-game-page'),
-    url(r'^game/(?P<pk>\d+)/join/$', 'join_game_feeder', name='core-join-game-feeder')
+    url(r'^game/(?P<pk>\d+)/join/$', 'join_game_feeder', name='core-join-game-feeder'),
+
+    url(r'^test-game/$', TemplateView.as_view(template_name='core/test_game.html')),
 )
