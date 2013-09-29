a10c4561f8bf332d6846b1fe64fb57d290f3adf3
fluder <fluder.tw@gmail.com>
2013-09-30 00:22:26 +0300

    Updated some comments

--- source/emulator/rules/fight_rules.py
+++ source/emulator/rules/fight_rules.py
@@ -11,24 +11,25 @@ class FightingRule(Rule):
     """
     def resolve(self, player, enemy):
         if isinstance(player.action, FightingAction):
+            # Perform energy changing
             if type(player.action) is PunchingAction:
                 player.queue_energy(player.energy-20)
             elif type(player.action) is KickingAction:
                 player.queue_energy(player.energy-40)
 
             if isinstance(enemy.action, BlockingAction):
-                print '%s blocked hit' % enemy
                 return  # Enemy blocked hit
 
             # Hit is not blocked
-            print "%s received damage %d" % (enemy, player.action.damage)
             if type(player.action) is PunchingAction:
                 enemy.queue_action(GettingHitByPunchAction())
             elif type(player.action) is KickingAction:
                 enemy.queue_action(GettingHitByKickAction())
 
+            # Decrease healths
             enemy.queue_damage(player.action.damage)
 
+            # Enemy died
             if enemy.health <= 0:
                 if type(player.action) is PunchingAction:
                     enemy.queue_action(FallingByPunchAction())
@@ -37,6 +38,9 @@ class FightingRule(Rule):
 
 
 class VictoryRule(Rule):
+    """
+    Rule for changing players action to VictoryAction
+    """
     def resolve(self, player, enemy):
         if isinstance(player.action, FallingAction) and enemy.action != enemy._queued_action:
             enemy.queue_action(VictoryAction())
