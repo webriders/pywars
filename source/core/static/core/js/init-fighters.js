c5354a63742b8a066149c1b61075672ba529831f
Nikolay <koval.nikolay@gmail.com>
2013-09-29 18:02:47 +0200

    Added preloader

--- source/core/static/core/js/init-fighters.js
+++ source/core/static/core/js/init-fighters.js
@@ -1,22 +1,17 @@
-$(function(){
+$(function () {
   var f1 = new pywars.Fighter('scorpion', 1);
   var f2 = new pywars.Fighter('scorpion', 2);
-  pywars.Arena.initStage();
 
+  pywars.Arena.initStage();
   pywars.Arena.addFighter(f2);
   pywars.Arena.addFighter(f1);
 
-  setTimeout(function(){
-    pywars.Arena.play(jQuery.extend({},SCENARIO))
+  for (var i in SCENARIO) {
+    var round = SCENARIO[i];
+    console.log(round)
+    pywars.Arena.play($.parseJSON(round['scene']));
+  }
 
-
-
-  }, 1000);
-
-  setTimeout(function(){
-    pywars.Arena.play(jQuery.extend({},SCENARIO))
-}, 7000);
-
-  $('#stage').on('scenario.end', function(){
+  $('#stage').on('scenario.end', function () {
   })
 })
