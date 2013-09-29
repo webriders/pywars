cf475cb4fee80d9ef971a14a3403ba556e06f4f2
fluder <fluder.tw@gmail.com>
2013-09-29 11:23:05 +0300

    Added initial logic for animating fighting

--- source/core/urls.py
+++ source/core/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns(
     url(r'^game/(?P<pk>\d+)/code/$', 'submit_code_action', name='core-submit-code-action'),
 
     # Feeders
-    url(r'^game/(?P<game_pk>\d+)/round/(?P<last_round_number>\d+)/$', 'new_rounds_feed', name='core-new-rounds-feed'),
+    url(r'^game/(?P<pk>\d+)/round/(?P<last_round_number>\d+)/$', 'new_rounds_feed', name='core-new-rounds-feed'),
     url(r'^game/(?P<pk>\d+)/state/$', 'game_state_feed', name='core-game-state-feed'),
 
     url(r'^test-game/$', TemplateView.as_view(template_name='core/test_game.html')),
